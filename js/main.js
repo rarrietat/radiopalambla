0f80e34dbe85a2010a368488c89a9f810d955d84
Rafael Arrieta T <rarrietat@gmail.com>
2020-07-01 00:15:01 -0500

    radio palambla v1

---
+++ js/main.js
@@ -0,0 +1,117 @@
+/* Efecto scroll header */
+window.addEventListener("scroll", function () {
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
+
+/* Reproductor de música */
+jQuery(function () {
+    jQuery("#lbg_audio9_html5_radio_zuper_1").audio9_html5({
+        radio_stream: "http://stream.rockradio.si:9034/;",
+        radio_name: "Your Radio Title Here",
+        playerWidth: 355,
+        imageHeight: 355,
+        skin: "blackControllers",
+        responsive: true,
+        grabLastFmPhoto: true,
+        autoPlay: true,
+        songTitleColor: "#ffffff",
+        authorTitleColor: "#ffffff",
+        lineSeparatorColor: "#ffffff",
+        radioStationColor: "#ffffff",
+        frameBehindTextColor: "#000000",
+        buttonsOffState: "#ffffff",
+        buttonsOnState: "#00AFEF",
+        sticky: false,
+        startMinified: false,
+        centerPlayer: false,
+        playerBorderSize: 0,
+        playerBorderColor: "#000000",
+        showFacebookBut: true,
+        facebookAppID: "",
+        facebookShareTitle: "HTML5 Radio Player With History - Shoutcast and Icecast",
+        facebookShareDescription: "A top-notch responsive HTML5 Radio Player compatible with all major browsers and mobile devices.",
+        facebookShareImage: "",
+        showTwitterBut: true,
+        showVolume: true,
+        showBio: true,
+        translateBio: "Bio",
+        bioAndLyricsColor: "#000000",
+        showRadioStation: true,
+        showTitle: true,
+        showHistoryBut: true,
+        translateReadingData: "reading data...",
+        historyTranslate: "HISTORY - latest played songs",
+        historyTitleColor: "#008aa4",
+        historyBgColor: "#FFFFFF",
+        historyBgColorAlpha: 0.9,
+        historyRecordBgColor: "transparent",
+        historyRecordBottomBorderColor: "transparent",
+        historyRecordSongColor: "#000000",
+        historyRecordSongBottomBorderColor: "#008aa4",
+        historyRecordAuthorColor: "#6d6d6d",
+        historyRecordThumbBorderColor: "#000000",
+        numberOfThumbsPerScreen: 3,
+        historyPadding: 16,
+        historyRecordTitleLimit: 25,
+        historyRecordAuthorLimit: 36,
+        pathToAjaxFiles: "lbg-audio9_html5_radio_zuper/audio9_html5_radio_zuper/",
+        nowPlayingInterval: 40,
+        noImageAvailable: "lbg-audio9_html5_radio_zuper/audio9_html5_radio_zuper/noimageavailable.jpg",
+        textureOverImage: "lbg-audio9_html5_radio_zuper/audio9_html5_radio_zuper/controllers/texture4x4_1.png"
+    });
+});
+
+/* Carousel de auspiciadores */
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 20,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    })
+});
+
+
+/* Smooth Scroll */
+let buttons = document.querySelectorAll('a[href^="#"]');
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        let target = e.currentTarget.getAttribute('href');
+
+        document.querySelector(target).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+/* Toggle Menu */
+const toggleCollapse = document.querySelector('.icon');
+const nav = document.querySelector('nav');
+let links = document.querySelectorAll('.menu a');
+
+toggleCollapse.onclick = (e) =>{
+    e.target.classList.toggle('active');
+    nav.classList.toggle('active');
+    links.forEach((link) => {
+        link.onclick = () => {
+            nav.classList.remove('active');
+            e.target.classList.remove('active');
+            console.log(nav.classList);
+        };
+    });
+}
